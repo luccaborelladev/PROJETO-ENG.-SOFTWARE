c35228805ac76c9636dce272893f08dd7099ca8e
Rafael Gusmão <rafael.g.mendonca0808@gmail.com>
2025-06-06 01:03:10 -0300

    modificações para a criação do banco de dados

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from models import db, Task
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db.init_app(app)
 
 # Criar o banco de dados
