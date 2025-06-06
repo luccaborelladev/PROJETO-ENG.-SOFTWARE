912b322545e8d5870f17a913921aa3d636e37734
Rafael Gusmão <rafael.g.mendonca0808@gmail.com>
2025-06-06 00:35:09 -0300

    realterei o app.py e o models.py

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ from models import db, Task
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 db.init_app(app)
 
 # Criar o banco de dados
--- models.py
+++ models.py
@@ -4,16 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Tarefa(db.Model):
-    tablename = 'tarefas'
-
+class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    titulo = db.Column(db.String(100), nullable=False)
-    status = db.Column(db.Boolean, default=False)
-
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "titulo": self.titulo,
-            "status": self.status
-        }
+    name = db.Column(db.String(100), nullable=False)
+    completed = db.Column(db.Boolean, default=False)
