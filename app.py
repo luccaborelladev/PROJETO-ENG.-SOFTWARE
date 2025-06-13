af73fd39e12e3448fee96e1b0757f7723c59e11c
Lucca Borella <luccatoledo2004@hotmail.com>
2025-06-12 21:06:08 -0300

    area de login e senha

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 #App principal com rotas Flask 
  # app.py
-from flask import Flask, request, jsonify
-from models import db, Task
-from flask import render_template  # adicione no topo junto com os imports
-
+# app.py
+from flask import Flask, render_template, request, jsonify, redirect, url_for, session
+from flask_sqlalchemy import SQLAlchemy
+from models import db, Task, User
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'chave_secreta_segura'
 
 db.init_app(app)
 
@@ -15,19 +17,18 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
-# Rota para a página inicial
 @app.route('/')
 def home():
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
     tasks = Task.query.all()
     return render_template('index.html', tasks=tasks)
 
-# Rota para obter todas as tarefas
 @app.route('/tasks', methods=['GET'])
 def get_tasks():
     tasks = Task.query.all()
     return jsonify([{'id': t.id, 'title': t.title, 'done': t.done} for t in tasks])
 
-# Rota para adicionar nova tarefa
 @app.route('/tasks', methods=['POST'])
 def add_task():
     data = request.get_json()
@@ -36,7 +37,6 @@ def add_task():
     db.session.commit()
     return jsonify({'id': new_task.id, 'title': new_task.title, 'done': new_task.done}), 201
 
-# Rota para atualizar uma tarefa
 @app.route('/tasks/<int:id>', methods=['PUT'])
 def update_task(id):
     task = Task.query.get_or_404(id)
@@ -46,7 +46,6 @@ def update_task(id):
     db.session.commit()
     return jsonify({'id': task.id, 'title': task.title, 'done': task.done})
 
-# Rota para deletar uma tarefa
 @app.route('/tasks/<int:id>', methods=['DELETE'])
 def delete_task(id):
     task = Task.query.get_or_404(id)
@@ -54,6 +53,86 @@ def delete_task(id):
     db.session.commit()
     return jsonify({'message': 'Tarefa deletada com sucesso!'})
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        data = request.form
+        username = data['username']
+        password = data['password']
+
+        if User.query.filter_by(username=username).first():
+            return "Usuário já existe!", 400
+
+        user = User(username=username)
+        user.set_password(password)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
+
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        data = request.form
+        username = data['username']
+        password = data['password']
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.check_password(password):
+            session['user_id'] = user.id
+            return redirect(url_for('home'))
+        return "Login inválido", 401
+
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    return redirect(url_for('login'))
+
+@app.route('/profile')
+def profile():
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+    user = User.query.get(session['user_id'])
+    return render_template('profile.html', user=user)
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+def edit_profile():
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+    user = User.query.get(session['user_id'])
+
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if username:
+            user.username = username
+        if password:
+            user.set_password(password)
+
+        db.session.commit()
+        return redirect(url_for('profile'))
+
+    return render_template('edit_profile.html', user=user)
+
+@app.route('/reset_password', methods=['GET', 'POST'])
+def reset_password():
+    if request.method == 'POST':
+        username = request.form['username']
+        new_password = request.form['new_password']
+
+        user = User.query.filter_by(username=username).first()
+        if not user:
+            return "Usuário não encontrado!", 404
+
+        user.set_password(new_password)
+        db.session.commit()
+        return redirect(url_for('login'))
+
+    return render_template('reset_password.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
-
